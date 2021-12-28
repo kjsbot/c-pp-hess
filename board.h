b82d10647ff3f71b1364b6b9970469fc9dfd4e22
Fiona W <62452684+kjsbot@users.noreply.github.com>
2021-12-28 18:46:44 -0500

    Update board.h

--- board.h
+++ board.h
@@ -28,7 +28,6 @@ class Board {
 
 		bool validateInput(string input, int color);
 		void inputToCoor(string input);
-		//string coorToString(coordinate output);
 		
 		bool isCheck();
 		bool isCheckmate();
@@ -41,13 +40,13 @@ class Board {
 		coordinate targetPos {0, 0};
 		coordinate currentPos {0, 0};
 
-		vector<vector<int>> legalMoves();
-		vector<vector<int>> opponentMoves();
+		vector<vector<int>> opponentMoves(); // checking for stalemate, checkmate
 
-		//<vector<vector<int>> diagonalMovement();
-		//<vector<vector<int>> updownMovement();
-		vector<int> knightLogic(string curPos, int pieceColor);
-		//<vector<vector<int>> pawnLogic();
+		vector<vector<int>> diagonalMovement(string curPos, int pieceColor);
+		vector<vector<int>> updownMovement(string curPos, int pieceColor);
+		vector<vector<int>> kingLogic(string curPos, int pieceColor);
+		vector<vector<int>> pawnLogic(string curPos, int pieceColor);
+		vector<vector<int>> knightLogic(string curPos, int pieceColor);
 
 		//bool enPassant();
 		//bool canCastle();
