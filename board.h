ed4f374045292715740f4210a00d89fe77acfca7
Fiona W <62452684+kjsbot@users.noreply.github.com>
2021-12-29 13:56:37 -0500

    Update board.h

--- board.h
+++ board.h
@@ -21,13 +21,15 @@ class Board {
 			int x;
 		};
 
-		int currentSide {1}; // 1 = white, -1 = black
+		int currentSide {-1}; // -1 = white, 1 = black
 
 		void display();
 		void newGame();
+		void inputToCoor(string input);
 
 		bool validateInput(string input, int color);
-		void inputToCoor(string input);
+		bool pseudoMovesChecker(string input);
+		bool legalMovesChecker(string input, int color);
 		
 		bool isCheck();
 		bool isCheckmate();
@@ -36,20 +38,33 @@ class Board {
 	private:
 		Square* getPiece();
 
-		void movePiece(coordinate currentPos, coordinate targetPos);
+		void movePiece(string input);
 		coordinate targetPos {0, 0};
 		coordinate currentPos {0, 0};
 
 		vector<vector<int>> opponentMoves(); // checking for stalemate, checkmate
 
-		vector<vector<int>> diagonalMovement(string curPos, int pieceColor);
-		vector<vector<int>> updownMovement(string curPos, int pieceColor);
-		vector<vector<int>> kingLogic(string curPos, int pieceColor);
-		vector<vector<int>> pawnLogic(string curPos, int pieceColor);
-		vector<vector<int>> knightLogic(string curPos, int pieceColor);
+		vector<vector<int>> diagonalMovement(int pieceColor);
+		vector<vector<int>> updownMovement(int pieceColor);
+		vector<vector<int>> kingLogic(int pieceColor);
+		vector<vector<int>> pawnLogic(int pieceColor);
+		vector<vector<int>> knightLogic(int pieceColor);
 
-		//bool enPassant();
-		//bool canCastle();
+		bool enPassant();
+		bool canCastle();
 };
 
 #endif
+
+/*
+int board[8][8] = 
+	{ {0, 1, 2, 3, 4, 5, 6, 7}, // Black
+	  {0, 1, 2, 3, 4, 5, 6, 7},
+	  {0, 1, 2, K, 4, 5, 6, 7},
+	  {0, 1, 2, 3, 4, 5, 6, 7},
+	  {0, 1, 2, 3, 4, 5, 6, 7},
+	  {0, 1, 2, 3, 4, 5, 6, 7},
+	  {0, 1, 2, 3, 4, 5, 6, 7},
+	  {0, 1, 2, 3, 4, 5, 6, 7} } // R K B Q KING B K R
+	};
+*/
